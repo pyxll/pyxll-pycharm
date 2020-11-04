55e23ea669b6ad22e70e41a95f05172f80fa02b3
Tony Roberts <tony@pyxll.com>
2020-11-04 11:51:31 +0000

    Make the user prompts clearer when reconnecting
    When the debugger connect has already been created prompt the user to
    remind them to restart the debug server.
    
    The debug server needs to be restarted in order to reconnect.

--- pyxll_pycharm/__init__.py
+++ pyxll_pycharm/__init__.py
@@ -27,6 +27,10 @@ import sys
 
 _log = logging.getLogger(__name__)
 
+_MB_YESNO = 0x04
+_MB_OK = 0x0
+_IDYES = 0x6
+
 
 def connect_to_pycharm(*args):
     """Connect to the remote PyCharm debugger."""
@@ -47,27 +51,50 @@ def connect_to_pycharm(*args):
         except (ValueError, TypeError):
             _log.error("Unexpected value for PYCHARM.suspend.")
 
-    MB_YESNO = 0x04
-    MB_OK = 0x0
-    IDYES = 0x6
-    result = ctypes.windll.user32.MessageBoxA(
-        0,
-        b"Have you started the PyCharm remote debugger on port %d?" % port,
-        b"PyCharm Remote Debug",
-        MB_YESNO)
-
-    if result != IDYES:
-        ctypes.windll.user32.MessageBoxA(
+    # If the debugger is not already running ask the user if they have started the debug server
+    if not pydevd.connected:
+        result = ctypes.windll.user32.MessageBoxA(
             0,
-            b"Please start the PyCharm remote debugger on port %d first." % port,
+            b"Have you started the PyCharm remote debugger on port %d?" % port,
             b"PyCharm Remote Debug",
-            MB_OK)
-        return
+            _MB_YESNO)
 
-    # If the debugger was already running then stop it, ready to reconnect
-    if pydevd.connected:
+        if result != _IDYES:
+            ctypes.windll.user32.MessageBoxA(
+                0,
+                b"Please start the PyCharm remote debugger on port %d first." % port,
+                b"PyCharm Remote Debug",
+                _MB_OK)
+            return
+    else:
+        # The debugger is already running so check if the user has restarted the debug server
+        result = ctypes.windll.user32.MessageBoxA(
+            0,
+            b"The PyCharm debugger was already connected.\n" +
+            b"Have you re-started the PyCharm remote debugger on port %d?" % port,
+            b"PyCharm Remote Debug",
+            _MB_YESNO)
+
+        if result != _IDYES:
+            ctypes.windll.user32.MessageBoxA(
+                0,
+                b"Please re-start the PyCharm remote debugger on port %d first." % port,
+                b"PyCharm Remote Debug",
+                _MB_OK)
+            return
+
+        # Try to stop the current debugger (this sets pydevd.connected to False)
         _log.info("Stopping the PyCharm debugger connection...")
         pydevd.stoptrace()
+
+        # Undo the stdout/stderr redirection (not strictly necessary!)
+        if hasattr(sys, "_pydevd_out_buffer_") and hasattr(sys, "stdout_original"):
+            sys.stdout = sys.stdout_original
+            del sys._pydevd_out_buffer_
+
+        if hasattr(sys, "_pydevd_err_buffer_") and hasattr(sys, "stderr_original"):
+            sys.stderr = sys.stderr_original
+            del sys._pydevd_err_buffer_
 
     # Connect to the remote debugger
     pydevd_pycharm.settrace("localhost",
@@ -83,7 +110,7 @@ def connect_to_pycharm(*args):
         0,
         b"You are now connected to the PyCharm debugger.",
         b"PyCharm Remote Debug",
-        MB_OK)
+        _MB_OK)
 
 
 def modules():
